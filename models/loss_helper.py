7bf817cc8c0f93992acca998026cde90501a0e71
Hoang Cuong <votegrasp@gmail.com>
2020-11-21 19:20:06 +0100

    heading to angle

--- models/loss_helper.py
+++ models/loss_helper.py
@@ -42,9 +42,6 @@ def compute_vote_loss(end_points):
     seed_gt_votes_mask = torch.gather(end_points['vote_label_mask'], 1, seed_inds)
     seed_inds_expand = seed_inds.view(batch_size,num_seed,1).repeat(1,1,3*GT_VOTE_FACTOR)
     seed_gt_votes = torch.gather(end_points['vote_label'], 1, seed_inds_expand)
-    print('cuong0:', seed_gt_votes.shape)
-    print('cuong1:', end_points['seed_xyz'].shape)
-    print('cuong2:', end_points['seed_xyz'].repeat(1,1,3).shape)
     seed_gt_votes += end_points['seed_xyz'].repeat(1,1,GT_VOTE_FACTOR)
 
     # Compute the min of min of distance
